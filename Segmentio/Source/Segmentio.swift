5664c835b823895bb68fd430eec78ec59f9644b5
Umair Gillani <31413820+umairgillani94@users.noreply.github.com>
2019-10-11 20:06:24 +0500

    Update Segmentio.swift

--- Segmentio/Source/Segmentio.swift
+++ Segmentio/Source/Segmentio.swift
@@ -58,7 +58,7 @@ open class Segmentio: UIView {
     }
     private var isFlipped = false
     
-    func changeFlipped (shouldFlip: Bool) {
+    public func changeFlipped (shouldFlip: Bool) {
         self.isFlipped = shouldFlip
     }
     
